4bff2ad76880f1ef8c93eff3382c7fb0b12e11a0
tasnimnova-28 <tasnimnova12521132@gmail.com>
2026-04-20 11:39:31 +0600

    Pattern2

--- pattern1.c
+++ pattern1.c
@@ -1,12 +1,15 @@
 #include<stdio.h>
+
 int main(){
-    int N;
-    scanf("%d", &N);
-    for(int i=1; i<=N; i++){
-        for(int j=1; j<=i; j++){
-            printf("%d ",j);
+    int n;
+    scanf("%d", &n);
+
+    for(int i = n; i >= 1; i--){
+        for(int j = i; j >= 1; j--){
+            printf("%d ", j);
         }
         printf("\n");
     }
+
     return 0;
 }
