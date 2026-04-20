8c38f45d9c0c16d0fcada106c60227da227827e5
tasnimnova-28 <tasnimnova12521132@gmail.com>
2026-04-20 11:23:11 +0600

    Lucky Numbers

---
+++ I_Lucky_Numbers.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(){
+    int n;
+    scanf("%d", &n);
+
+    int a = n / 10;
+    int b = n % 10;
+
+    if((b != 0 && a % b == 0) || (a != 0 && b % a == 0)){
+        printf("YES\n");
+    }
+    else{
+        printf("NO\n");
+    }
+
+    return 0;
+}
